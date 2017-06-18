795e862b3f8eb4b5fed8e97a351a0dcc6168ef3e
Nargonath <jonas.pauthier@gmail.com>
2017-06-18 16:19:41 +0200

    Fixes #17: Added Notification package to the project and added the global ui-router error catch for state transition

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import uiRouter from '@uirouter/angularjs';
 import oclazyload from 'oclazyload';
+import uiNotification from 'angular-ui-notification';
 
 import './commons/bootstrap.scss';
 import Components from './components';
@@ -9,6 +10,7 @@ angular.module('myApp', [
   uiRouter,
   Components,
   oclazyload,
+  uiNotification,
 ])
 .config(($locationProvider, $urlRouterProvider) => {
   'ngInject';
@@ -18,4 +20,13 @@ angular.module('myApp', [
   if (process.env.NODE_ENV === 'prod') {
     $locationProvider.html5Mode(true);
   }
+})
+.run(($state, Notification, $log) => {
+  'ngInject';
+
+  $state.defaultErrorHandler((error) => {
+    Notification.error({  message: `You cannot perform this operation` });
+    const { message, statusCode } = error.detail.data;
+    $log.error(`Erreur ${statusCode} : ${message}`);
+  });
 });

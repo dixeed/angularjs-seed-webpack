9465242b45e27d74ddcc4b483feb67017cb0fd80
Nargonath <jonas.pauthier@gmail.com>
2017-06-27 00:31:21 +0200

    Fixes #32: Added object rest/spread operator

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -2,15 +2,21 @@ import data from './data.json';
 import imgTest from 'resolve-test.jpg';
 
 export default class HomeComponent {
-  constructor() {
+  constructor($log) {
     'ngInject';
 
     this.content = data.content;
     this.subTitle = data.title;
     this.imgSrc = imgTest;
+    this.$log = $log;
   }
 
   $onInit() {
     this.input = '';
+    // object rest/spread test
+    let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
+    this.$log.log(x); // 1
+    this.$log.log(y); // 2
+    this.$log.log(z); // { a: 3, b: 4 }
   }
 }
